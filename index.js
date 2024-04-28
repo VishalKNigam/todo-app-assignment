589aee7925eeb9bbfd9b166b1fca8b3f866ec66f
Vishal Kumar Nigam <115461865+VishalKNigam@users.noreply.github.com>
2024-04-28 17:05:58 +0530

    Client side done

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const { connection } = require("./db");
 const UserRouter = require("./routes/user.route");
+const TodoRouter = require("./routes/todo.route");
+const path = require("path");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-app.use("/api/auth".UserRouter);
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.get("/api", (req, res) => {
+    res.send("Todo-app-assignment");
+});
+
+app.use("/api/auth", UserRouter);
+app.use("/api/todos", TodoRouter);
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.get("/", (req, res) => {
     res.send({ msg: "Add your task, Manage your task. I am here to assist you!😊" });
 
 });
-app.listen(process.env.PORT, async()=>{
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+app.listen(process.env.PORT, async () => {
     console.log(`Server is running at http://localhost:${process.env.PORT}`);
     try {
         await connection;
--- models/todo.model.js
+++ models/todo.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const TodoSchema = mongoose.Schema({
+const ToDoSchema = mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     content: { type: String, required: true },
     complete: { type: Boolean, default: false },
     completedAt: { type: Date }
 }, { timestamps: true });
-const TodoModel = mongoose.model("ToDo", TodoSchema);
+const TodoModel = mongoose.model("ToDo", ToDoSchema);
 module.exports = { TodoModel };
